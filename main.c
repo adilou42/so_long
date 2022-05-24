36b5faf2de1a918efaf7db7d91f4bdb925ad4d48
Adil YAKDI <ayakdi@e3r8p11.clusters.42paris.fr>
2022-05-24 21:05:06 +0200

    :seedling: square display

--- main.c
+++ main.c
@@ -6,19 +6,64 @@
 /*   By: ayakdi <ayakdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 20:17:59 by ayakdi            #+#    #+#             */
-/*   Updated: 2022/05/24 15:53:45 by ayakdi           ###   ########.fr       */
+/*   Updated: 2022/05/24 21:02:13 by ayakdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include "./mlx/mlx.h"
 
-void	my_mlx_pixel_put(t_map *map, int x, int y, int color)
+void	render_background(t_data *data, int color)
 {
-	char	*dst;
+	int	i;
+	int	j;
 
-	dst = map->addr + (y * map->line_length + x * (map->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	if (data->win_ptr == NULL)
+		return ;
+	i = 0;
+	while (i < 500)
+	{
+		j = 0;
+		while (j < 1000)
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, color);
+		++i;
+	}
+}
+
+int render_rect(t_data *data, t_rect rect)
+{
+	int	i;
+	int j;
+
+	if (data->win_ptr == NULL)
+		return (1);
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
+		++i;
+	}
+	return (0);
+}
+
+int	render(t_data *data)
+{
+
+	render_background(data, FFFFFF);
+	render_rect(data, (t_rect){900, 400, 100, 100, 0xFF00});
+	render_rect(data, (t_rect){0, 0, 100, 100, 0x00FF0000});
+
+	return (0);
+}
+
+void	img_pixel_put(t_img *image, int x, int y, int color)
+{
+	char	*pixel;
+
+	pixel = image->addr + (y * image->line_len + x * (image->bpp / 8));
+	*(int *)pixel = color;
 }
 
 int	handle_keypress(int keysym, t_data *data)
@@ -31,30 +76,13 @@ int	handle_keypress(int keysym, t_data *data)
 	return (0);
 }
 
-int	handle_keyrelease(void	*data)
-{
-	(void)data;
-	return (0);
-}
 
-int	handle_no_event(void *data)
-{
-	(void)data;
-	return (0);
-}
-
-int	handle_input(int keysym, t_data *data)
-{
-	if (keysym == XK_Escape)
-		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-	return (0);
-}
 
 int main(void)
 {
 
 	t_data	data;
-	t_map	img;
+	t_img	image;
 
 	data.count = 0;
 	data.mlx_ptr = mlx_init();
@@ -69,15 +97,9 @@ int main(void)
 		return (0);
 	}
 
-	img.img = mlx_new_image(data.mlx_ptr, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 50, 50, 0x00FF0000);
-	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, img.img, 0, 0);
-
-
-	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+	image.mlx_img = mlx_new_image(data.mlx_ptr, 1000, 500);
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
-	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data);
 
 	mlx_loop(data.mlx_ptr);
 
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: ayakdi <ayakdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 16:12:42 by ayakdi            #+#    #+#             */
-/*   Updated: 2022/05/24 14:51:19 by ayakdi           ###   ########.fr       */
+/*   Updated: 2022/05/24 21:02:13 by ayakdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,23 @@ typedef struct s_data
 	int		count;
 }	t_data;
 
-typedef struct	s_map {
-	void	*img;
+typedef struct s_img
+{
+	void	*mlx_img;
 	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
+	int		bpp; /* bits per pixel */
+	int		line_len;
 	int		endian;
-}				t_map;
+}	t_img;
+
+typedef struct s_rect
+{
+	int	x;
+	int	y;
+	int width;
+	int height;
+	int color;
+}	t_rect;
 
 int	handle_no_event(void *data);
 int	handle_input(int keysym, t_data *data);
